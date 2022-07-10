7e5abe597b3d89d5fea43ed31081175577ae34cd
12sdj <103876733+12sdj@users.noreply.github.com>
2022-07-10 14:01:05 +0800

    Add files via upload

---
+++ MouseClicker Basic/Version 3.5.7/MouseClicker Basic.cpp
@@ -0,0 +1,406 @@
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdio>
+#include<thread>
+#include<cstdlib>
+#include<bits/stdc++.h>
+#include<random>
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0) 
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+
+int a;
+long c;
+long long b; 
+int kill = 1;
+
+void Test()
+{
+	HWND hWnd = GetConsoleWindow();
+	HMENU hMenu = GetSystemMenu(hWnd, false);
+	RemoveMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+	DestroyMenu(hMenu);
+	ReleaseDC(hWnd, NULL);
+}
+
+void func2(){
+
+	while(1){
+        if(GetAsyncKeyState(VK_ESCAPE)){
+        	printf("%s","<Trying to stop the program... >\n");
+        	kill = 0;		
+    }
+}
+
+}
+void func()
+{
+	//			if(MessageBox(NULL,"Still Continue?","MouseClicker",MB_YESNO+32)==6)
+	thread t(func2);
+	t.detach();
+	pid_t pid = getpid();
+
+	if(kill == 1){
+		while (1) {
+			default_random_engine e;
+	   		uniform_real_distribution<double> u(-8.0, 15.0);
+			u(e);
+			int(u(e));
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 500, 500, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, 500, 500, 0, 0);
+			Sleep(c+u(e)); 
+				
+		}
+	}
+	else{
+		system("taskkill /f /t /im MouseClicker Basic.exe");
+		system("taskkill /f /t /pid "+ pid);
+	}
+}
+
+void funk()
+{
+	thread t(func2);
+	t.detach();
+	pid_t pid = getpid();
+
+	if(kill == 1){
+		while (1) {
+			default_random_engine e;
+	   		uniform_real_distribution<double> u(-8.0, 15.0);
+			u(e);
+			int(u(e));
+			mouse_event(MOUSEEVENTF_RIGHTDOWN, 500, 500, 0, 0);
+			mouse_event(MOUSEEVENTF_RIGHTUP, 500, 500, 0, 0);
+			
+			Sleep(c+u(e)); 
+					
+						
+		}
+}
+	else{
+		system("taskkill /f /t /im MouseClicker Basic.exe");
+		system("taskkill /f /t /pid "+ pid);
+	}
+}
+
+void funs()
+{
+	thread t(func2);
+	t.detach();
+	pid_t pid = getpid();
+
+	if(kill == 1){
+		while (1) {
+			default_random_engine e;
+	   		uniform_real_distribution<double> u(-8.0, 15.0);
+			u(e);
+			int(u(e));
+			mouse_event(MOUSEEVENTF_MIDDLEDOWN, 500, 500, 0, 0);
+			mouse_event(MOUSEEVENTF_MIDDLEUP, 500, 500, 0, 0);
+			
+			Sleep(c+u(e)); 
+					
+						
+		}
+	}
+	else{
+		system("taskkill /f /t /im MouseClicker Basic.exe");
+		system("taskkill /f /t /pid "+ pid);
+	}
+}
+
+
+int main()
+{
+	Test();
+	int ch;
+	int af;
+	int kh;
+
+	while (true){
+		a = 0;
+		b = 0;
+		c = 0;
+		af = 0;
+		printf("%s","MouseClicker Basic\n");
+		printf("%s","Program Version 3.5.7(build 3132BC_220710) Release\n");
+		printf("%s","Compiler Version TDM-GCC 9.2.0 64-bit Release\n");
+		printf("%s","Copyright (c) 2022 12sdj\n");
+		printf("%s","More information: https://github.com/12sdj/MouseClicker-by-12sdj\n");
+		printf("%s","Checking updates: https://github.com/12sdj/MouseClicker-by-12sdj/releases\n");
+		cout << endl;
+		printf("%s","Please note: This update is the last functional update in the series\n");
+
+	
+	
+		cout << endl;
+		cout << endl;
+		printf("%s","What's new (compared to the previous version): \n [Version 3.5(Update 20) Made by 12sdj]\n");
+		printf("%s","1.Fix one or more bug(s)\n");
+		printf("%s","2.Some formulations have been revised, and we deleted some error descriptions\n");
+		printf("%s","3.Increase the speed of the program\n");
+		printf("%s","4.Improve the overall stability of the program, improve the efficient use of the program CPU and RAM\n");
+		cout << endl;
+		system("color 02");
+		printf("%s","About 4s later, This procedure will begin to start.\n");
+		Sleep(4150);
+		
+		system("cls");
+		system("title MouseClicker Basic V3.5");
+		printf("%s","Settings\n");
+		cout << endl;
+		printf("%s","CPS, is the abbreviation of 'Click per second'. The Chinese translation is the number of clicks per second, which can also be understood as hand speed. The average person's CPS does not exceed 10. Using tools such as a connector can make the CPS exceed this number, and the limit can even reach 60.\n");
+		cout << endl;
+		printf("%s","Please input CPS[CPS <= 120]: ");
+	
+		cin >> b;
+		cout << endl;
+		
+	
+	
+  		if(b <= 0) {
+        	printf("<MouseClicker Basic: Error>\n");
+			MessageBox(NULL,"Program Error!                                ","MouseClicker",MB_OK+16);
+			MessageBox(NULL,"Please enter an appropriate positive number!  ","MouseClicker",MB_OK+48);
+			MessageBox(NULL,"Program Crashes!                              ","MouseClicker",MB_OK+16);
+			return false;
+	}
+		
+		c = 1000/b;
+		system("cls");
+		
+		printf("%s","Settings\n");
+		cout << endl;
+		cout << "CPS: " << b <<endl;
+		cout << endl;
+		Sleep(500);
+		cout << "Please input 'left' or 'right' or 'middle' ,If you want to click the left mouse button, enter 'left', if you want to click the right mouse button, enter 'right',if you want to click the middle mouse button, enter'middle' "<<endl;
+		cout << "Please input your choose:  ";
+		char put[5];
+		int left,right,middle;
+		cin >> put;
+		
+		
+		
+		
+		system("cls");
+		cout <<"Settings" << endl;
+		cout << endl;
+		cout << "CPS: " << b <<endl;
+		cout << "Click: " << put << endl;
+
+		
+		Sleep(1500);
+		cout << endl;
+		cout << "Please input any key to start: ";
+		cin >> a;
+		system("cls"); 
+		
+		cout << "<Processing request>" << endl;
+		cout << "Processing request";
+		system("color 02");
+		for (int y =1; y <= 3; y++){
+			cout << ".";
+			Sleep(950);
+		}
+		cout << endl;
+		system("cls");
+		cout << "MouseClicker Basic" << endl;
+
+		system("color");
+		
+		cout << endl;
+		printf("%s","Checking your pleasure");
+		for(int i=1; i<=3; i++){
+			printf("%s",".");
+			Sleep(400);
+		}
+		cout << endl;
+		Sleep(500);
+
+		if(strcmp(put, "left") == 0){
+			af = 1;
+		}
+		if(strcmp(put, "right") == 0){
+			af = 2;
+		}
+		if(strcmp(put, "middle") == 0){
+			af = 3;
+		}
+		if(strcmp(put, "left") != 0 and strcmp(put, "right") != 0 and strcmp(put, "middle") != 0){
+			af = 4;
+		}
+		printf("%s","<The main program is creating and splitting processes>\n");
+			
+		pid_t pid1 = getpid();
+		//string apkill = "taskkill /f /t /im \"MouseClicker Basic.exe\"";
+		
+		
+		
+		if(af == 1){
+			thread t(func);
+			t.detach();
+			cout << "<WAITING>" << endl;
+			Sleep(2000);
+			system("cls");
+			printf("%s","<Execution>\n");
+			printf("%s","Tips: You need to press ESC to EXIT the program\n");
+			
+			
+			while(1){
+				/*
+				if(kill == 0){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+					return 0;
+				}
+				if(GetAsyncKeyState(VK_ESCAPE)){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+				}*/
+				if (_kbhit()){
+            		ch = _getch();
+           			
+            		if (ch == 27){
+						system("taskkill /f /t /im \"MouseClicker Basic.exe\"");
+						system("taskkill /f /t /pid "+ pid1 );
+					}
+					
+        }
+        		if(kill == 0){
+			
+					return 0;
+			
+				
+
+			}
+				
+			
+				
+
+			}
+	}
+	
+		
+		if(af == 2){
+
+
+
+			thread k(funk);
+			k.detach();
+			cout << "<WAITING>" << endl;
+			Sleep(2000);
+			system("cls");
+	 
+			printf("%s","<Execution>\n");
+			printf("%s","Tips: You need to press ESC to EXIT the program\n");
+			
+			
+			
+			
+			while(1){
+				/*
+				if(kill == 0){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+					return 0;
+				}
+				if(GetAsyncKeyState(VK_ESCAPE)){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+				}*/
+				if (_kbhit()){
+            		ch = _getch();
+           			
+            		if (ch == 27){
+						system("taskkill /f /t /im \"MouseClicker Basic.exe\"");
+						system("taskkill /f /t /pid "+ pid1 );
+					}
+        }
+    		    if(kill == 0){
+			
+					return 0;
+			
+				
+
+			}
+				
+
+		}
+	
+}
+	
+				
+				
+
+		if(af == 3){
+			
+			thread s(funs);
+			s.detach();
+		
+			cout << "<WAITING>" << endl;
+			Sleep(2000);
+			system("cls");
+			printf("%s","<Execution>\n");
+			printf("%s","Tips: You need to press ESC to EXIT the program\n");
+			while(1){
+				/*
+				if(kill == 0){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+					return 0;
+				}
+				if(GetAsyncKeyState(VK_ESCAPE)){
+					system("taskkill /f /t /im MouseClicker Basic.exe");
+					system("taskkill /f /t /pid "+ pid1 );
+				}*/
+				if (_kbhit()){
+            		ch = _getch();
+           			
+            		if (ch == 27){
+						system("taskkill /f /t /im \"MouseClicker Basic.exe\"");
+						system("taskkill /f /t /pid "+ pid1 );
+					}
+        }
+			
+				if(kill == 0){
+			
+					return 0;
+			
+				
+
+				}
+
+			}
+				
+
+		} 
+	
+		
+
+	
+		if(af == 4){
+			system("color 04");
+			cout <<"ERROR!PLEASE INPUT 'LEFT' or 'RIGHT' or 'MIDDLE'" <<endl;
+			MessageBox(NULL,"ERROR!PLEASE INPUT 'LEFT' or 'RIGHT' or 'MIDDLE'. Please try again!'.","MouseClicker",MB_OK+16);
+			cout << "<Restart>" << endl;
+			for (int res = 1; res <= 5; res++){
+				cout << "</Restarting,Please Wait .../> " << "</There are" << 6-res << "seconds left/>" << endl;
+				Sleep(970);
+				system("cls");
+			}
+			Sleep(1550);
+			system("cls");
+	}
+			
+		
+}
+
+}
